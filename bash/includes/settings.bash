487bc9f5336ef41e2df515fd429e628cc4447890
wellsjo <wellsjohnston@gmail.com>
2014-06-21 12:25:15 -0400

    added a command to show available shortcuts and settings

---
+++ bash/includes/settings.bash
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+wells_settings() {
+    echo "${CYAN}##################################"
+    echo "############### Yo ###############"
+    echo "##################################"
+    echo -e "\n${BLUE}Hello.  Enjoy my setup plz.\n${WHITE}"
+    echo -e "SSH shortcuts:"
+    echo "${YELLOW}wells: ssh to my website vm"
+    echo "ppl: ssh to my dev vm"
+    echo "vm: ssh to my spotify vm"
+    echo "jump: ssh to jumphost"
+
+    echo "${WHITE}"
+}
