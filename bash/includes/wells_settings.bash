b225faa659b8c5cc4065eae6323f662353b1a630
wellsjo <wellsjohnston@gmail.com>
2014-06-24 21:25:05 -0400

    added more to wells settings.  added fzf shortcuts.

---
+++ bash/includes/wells_settings.bash
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+wells_settings() {
+
+    echo -e "${CYAN}\nSSH shortcuts"
+    echo "${YELLOW}wells - ssh to my website vm"
+    echo "ppl   - ssh to my dev vm"
+    echo "vm    - ssh to my spotify vm"
+    echo "jump  - ssh to jumphost"
+
+    echo -e "\n${CYAN}Bash aliases${YELLOW}"
+    echo -e "..    - cd .."
+    echo "ll    - la -la"
+
+    echo -e "${CYAN}\nFZF (fuzzy search) shortcuts${YELLOW}"
+    echo "fe    - open file in vim"
+    echo "fd    - cd to directory"
+    echo "fh    - repeat history"
+    echo "fkill - kill process"
+
+
+    echo -e "\n${CYAN}Other commands${YELLOW}"
+    echo "wells_settings | wsettings"
+    echo "wells_update | wupdate"
+    echo "wellssh"
+
+    echo "${WHITE}"
+}
+
+alias wsettings="wells_settings"
