47aaefeed98156de4571727bee6f00b76855731d
wellsjo <wellsjohnston@gmail.com>
2014-06-25 00:00:27 -0400

    updated from running wells_update

--- bash/includes/functions.bash
+++ bash/includes/functions.bash
@@ -46,7 +46,7 @@ wells_push() {
         echo "Showing diff..."
         git diff
         git add .
-        git commit -m "updated from running wells_update"
+        git commit -m "updated from running wells_push"
         git push
         cd ..
     fi
