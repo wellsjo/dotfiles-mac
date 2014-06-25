f8f9d9990e3bcd39a6c6e5aa5de77ae34b9798f4
wellsjo <wellsjohnston@gmail.com>
2014-06-24 22:21:57 -0400

    updating from wupdate command

--- bash/includes/functions.bash
+++ bash/includes/functions.bash
@@ -21,12 +21,15 @@ function h.symlink() {
 }
 
 wells_update() {
-    . "${HOME}/.profile"
-    echo -e "\nRe-sourcing wells_dotfiles..."
+
     echo -e "Updating git repo...\n"
     git add .
     git commit -m "updating from wupdate command"
     git push
+
+    echo -e "\nRe-sourcing wells_dotfiles..."
+    . "${HOME}/.profile"
+
     echo -e "\nDone!"
 }
 alias wupdate="wells_update"
