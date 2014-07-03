560ea4100470a8d7bf33e20b5a5f361e2b15b55a
Wells Johnston <wellsjohnston@gmail.com>
2014-07-03 11:45:21 -0400

    Update prompt.sh

--- bash/includes/prompt.sh
+++ bash/includes/prompt.sh
@@ -7,7 +7,7 @@ BG="\[\033[1;32m\]"
 BY="\[\033[1;33m\]"
 BW="\[\033[1;37m\]"
 
-function parse_git_branch {
+function git_info {
 if [ -d ".git" ]; then
     gitstatus=$(git status | grep clean)
     if [[ ${#gitstatus} == 0 ]]; then
@@ -19,5 +19,5 @@ fi
 git branch --no-color 2> /dev/null | sed -e '/^[^*]/d' -e "s/* \(.*\)/μ\1${gitdirty}/"
 }
 
-export PS1="\n${BC}\u ${BW}| ${BY}\W ${BG}\$(parse_git_branch)\n${BR}>> ${BW}"
+export PS1="\n${BC}\u ${BW}| ${BY}\W ${BG}\$(git_info)\n${BR}>> ${BW}"
 export PS2="    continue > "
