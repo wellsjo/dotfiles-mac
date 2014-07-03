c4edb7432f25a37ea7ae91aee05a94767562e178
Wells Johnston <wellsjohnston@gmail.com>
2014-07-03 11:43:55 -0400

    updated from running wells_push

--- bash/includes/prompt.sh
+++ bash/includes/prompt.sh
@@ -7,13 +7,17 @@ BG="\[\033[1;32m\]"
 BY="\[\033[1;33m\]"
 BW="\[\033[1;37m\]"
 
-function parse_git_dirty {
-[[ $(git status 2> /dev/null | tail -n1) != "(clean)" ]] && echo "*"
-}
-
 function parse_git_branch {
-git branch --no-color 2> /dev/null | sed -e '/^[^*]/d' -e "s/* \(.*\)/(\1)$(parse_git_dirty)/"
+if [ -d ".git" ]; then
+    gitstatus=$(git status | grep clean)
+    if [[ ${#gitstatus} == 0 ]]; then
+        gitdirty=" [+]"
+    else
+        gitdirty=" (clean)"
+    fi
+fi
+git branch --no-color 2> /dev/null | sed -e '/^[^*]/d' -e "s/* \(.*\)/μ\1${gitdirty}/"
 }
 
-export PS1="${BC}\u ${BW}| ${BY}\W ${BG}\$(parse_git_branch)\n${BR}>> ${BW}"
+export PS1="\n${BC}\u ${BW}| ${BY}\W ${BG}\$(parse_git_branch)\n${BR}>> ${BW}"
 export PS2="    continue > "
