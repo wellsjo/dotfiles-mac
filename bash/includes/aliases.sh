df0caa7af76ac8559502ff3ed463a317f35e22fc
Wells Johnston <wellsjohnston@gmail.com>
2014-07-22 21:14:32 -0400

    updated from running wells_push

--- bash/includes/aliases.sh
+++ bash/includes/aliases.sh
@@ -11,6 +11,7 @@ alias vm="ssh -tA www.bananajams.cloud.spotify.net 'cd src/web-site && tmux -2 a
 alias jump="ssh -A jump1.lon.spotify.net"
 
 # General bash aliases
+alias h="cd ~"
 alias gs="git status"
 alias gl="git log"
 alias gco="git commit -m"
