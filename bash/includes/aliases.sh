d76f39c514eeb6db47f5a5515eb66be194f16669
Wells Johnston <wellsjohnston@gmail.com>
2014-09-22 00:43:37 -0400

    updated from running wells_push

--- bash/includes/aliases.sh
+++ bash/includes/aliases.sh
@@ -5,8 +5,8 @@ alias d="~/src/web-site/devify.sh --skip-language --auto"
 alias c="console cache:clear"
 
 # Custom ssh shortcuts
-alias wells="ssh -i ~/Google\ Drive/Important\ Documents\ And\ Pictures/Amazon\ Web\ Services/wells_2.pem ec2-user@54.83.193.197"
-alias ppl="ssh -i ~/Google\ Drive/Important\ Documents\ And\ Pictures/Amazon\ Web\ Services/wells_2.pem ec2-user@54.83.27.53"
+alias wells="ssh -i ~/Google\ Drive/Important/Misc\ Saved\ Files/Amazon\ Web\ Services/wells_2.pem ec2-user@54.83.193.197"
+alias ppl="ssh -i ~/Google\ Drive/Important/Misc\ Saved\ Files/Amazon\ Web\ Services/wells_2.pem ec2-user@54.83.27.53"
 alias vm="ssh -tA wells@www.bananajams.cloud.spotify.net 'cd src/web-site && tmux -2 attach; bash -l'"
 alias jump="ssh -A wells@jump1.lon.spotify.net"
 
