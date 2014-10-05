c80442d43ed3d46bff3ba894bafb93405c08728b
Wells Johnston <wellsjohnston@gmail.com>
2014-10-05 15:29:09 -0400

    updated from running wells_push

--- bash/includes/aliases.sh
+++ bash/includes/aliases.sh
@@ -6,7 +6,7 @@ alias c="console cache:clear"
 
 # Custom ssh shortcuts
 alias wells="ssh -i ~/Google\ Drive/Important/Misc\ Saved\ Files/Amazon\ Web\ Services/wells_2.pem ec2-user@54.83.193.197"
-alias passenger="ssh -i ~/Google\ Drive/Important/Misc\ Saved\ Files/Amazon\ Web\ Services/wells_2.pem ec2-user@54.164.53.196"
+alias passenger="ssh -i ~/Google\ Drive/Important/Misc\ Saved\ Files/Amazon\ Web\ Services/wells_2.pem ubuntu@54.164.53.196"
 
 alias vm="ssh -tA wells@www.bananajams.cloud.spotify.net 'cd src/web-site && tmux -2 attach; bash -l'"
 alias jump="ssh -A wells@jump1.lon.spotify.net"
