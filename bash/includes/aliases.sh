e9403b23f57707930f50d6876b88ec60c9df71a4
Wells Johnston <wellsjohnston@gmail.com>
2014-10-22 23:11:32 -0400

    updated from running wells_push

--- bash/includes/aliases.sh
+++ bash/includes/aliases.sh
@@ -14,6 +14,7 @@ alias jump="ssh -A wells@jump1.lon.spotify.net"
 
 # General bash/git aliases
 alias colors="git_prompt_color_samples"
+alias wells_colors="colors"
 alias h="cd ~"
 alias gs="git status"
 alias gl="git log"
