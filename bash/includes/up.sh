67b25fbdc0c7cef5aac5d18fdd1c1e9380ba8d1b
Wells Johnston <wellsjohnston@gmail.com>
2014-11-24 20:51:09 -0500

    updated from running wells_push

---
+++ bash/includes/up.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+function up( )
+{
+    LIMIT=$1
+    P=$PWD
+    for ((i=1; i <= LIMIT; i++))
+    do
+        P=$P/..
+    done
+    cd $P
+    export MPWD=$P
+}
+
+function back( )
+{
+    LIMIT=$1
+    P=$MPWD
+    for ((i=1; i <= LIMIT; i++))
+    do
+        P=${P%/..}
+    done
+    cd $P
+    export MPWD=$P
+}
