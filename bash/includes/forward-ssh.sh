d445de0c1d14dabde8e86d0c113671ddb37f38b5
Wells Johnston <wellsjohnston@gmail.com>
2014-12-08 11:13:50 -0500

    updated from running wells_push

---
+++ bash/includes/forward-ssh.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+# this is to save my ssh credentials that have been fowarded, so they can be picked up by a tmux session
+# basic strategy is to take ssh creds and save them as variables in "latestagent".  Then when a tmux session starts,
+# export those variables into the session
+
+SSHVARS="SSH_CLIENT SSH_TTY SSH_AUTH_SOCK SSH_CONNECTION DISPLAY"
+
+for var in ${SSHVARS} ; do
+  echo "export $var=\"$(eval echo '$'$var)\""
+done 1>$HOME/.ssh/latestagent
