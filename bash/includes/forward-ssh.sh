6b64e11766fbebd216336c262804f1e1f58bb867
Wells Johnston <wellsjohnston@gmail.com>
2015-02-11 08:02:20 -0500

    changes made in airplane, cleaned up readme and bash includes

--- bash/includes/forward-ssh.sh
+++ bash/includes/forward-ssh.sh
@@ -1,8 +1,12 @@
 #!/bin/sh
 
-# this is to save my ssh credentials that have been fowarded, so they can be picked up by a tmux session
+# Pass SSH credentials to a tmux session
+
+# This is to save my ssh credentials that have been fowarded, so they can be picked up by a tmux session
 # basic strategy is to take ssh creds and save them as variables in "latestagent".  Then when a tmux session starts,
 # export those variables into the session by typing executing "fixssh"
+
+# Wells Johnston <w@wellsjohnston.com>
 
 SSHVARS="SSH_CLIENT SSH_TTY SSH_AUTH_SOCK SSH_CONNECTION DISPLAY"
 
