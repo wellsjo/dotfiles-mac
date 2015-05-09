0c2d284f58141e6a827131acb54f4faf721241d7
Wells Johnston <wellsjohnston@gmail.com>
2015-05-09 16:18:32 -0400

    update script

--- bash/includes/personal-scripts.sh
+++ bash/includes/personal-scripts.sh
@@ -2,7 +2,8 @@
 
 # SSH shortcuts
 alias wells="ssh -i ~/Google\ Drive/Persisted\ Files/Amazon\ Web\ Services/wells_2.pem ec2-user@54.83.193.197"
-alias passenger="ssh -i ~/Google\ Drive/Persisted\ Files/Amazon\ Web\ Services/wells_2.pem ubuntu@54.164.53.196"
+
+alias wells2="ssh -i ~/Google\ Drive/Persisted\ Files/Amazon\ Web\ Services/wells.pem ubuntu@52.1.44.12"
 
 
 # Fuckery
