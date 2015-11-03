b7fb0d0cff69548c025a8a45d4fdef5e1cd4922a
Wells Johnston <wellsjohnston@gmail.com>
2015-11-03 14:00:26 -0500

    updated git aliases

--- bash/includes/aliases.sh
+++ bash/includes/aliases.sh
@@ -8,5 +8,12 @@ alias l="ls -lha"
 alias la="ls -lha"
 alias c="clear"
 alias ..="cd .."
+
+# git
+alias gs="git status"
+alias gd="git diff"
+alias gl="git log"
+
+# tmux
 alias t="tmux -2"
 alias ta="tmux -2 attach"
