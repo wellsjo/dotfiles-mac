8aace69a4d4bfa89e5de914fe95f7611d8b0198b
Wells Johnston <wellsjohnston@gmail.com>
2016-05-24 14:45:39 -0400

    updates to OSX start script

--- OSX/setup.sh
+++ OSX/setup.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
-#####################
-# Get set up on OSX #
-#####################
+######################
+# Get set up on OSX! #
+######################
+
+###
+# First you are going to want to accept XCode's terms/conditions
+# and download their developer tools (which inclues git)
+#
 
 # install homebrew
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
@@ -11,6 +16,7 @@
 brew install tmux
 brew install reattach-to-user-namespace
 brew install htop
+brew install git-extras
 
 # node/npm
 mkdir ~/.nave
@@ -18,6 +24,9 @@ cd ~/.nave
 wget http://github.com/isaacs/nave/raw/master/nave.sh
 sudo ln -s $PWD/nave.sh /usr/local/bin/nave
 nave usemain $(nave stable)
+
+# node modules
+npm i serve -g
 
 # programming languages
 brew install python
