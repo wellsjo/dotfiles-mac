468b19245cead14e61381005448e9087ae6d859e
Wells Johnston <wellsjohnston@gmail.com>
2016-12-09 11:14:58 -0500

    removed unused funcs, added liquidprompt to .gitignore

--- bash/includes/docker-scripts.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-docker_rm_containers() {
-  sudo docker rm -f $(sudo docker ps -a -q)
-}
-
-docker_rm_images() {
-  sudo docker rmi -f $(sudo docker images -q)
-}
--- bash/includes/functions.sh
+++ bash/includes/functions.sh
@@ -23,30 +23,3 @@ function back( )
   cd $P
   export MPWD=$P
 }
-
-# Simple wrapper for ssh which makes wells_update() available in the remote session
-# regardless of whether .dotfiles is present remotely or not
-sshw() {
-  local func=$(typeset -f wells_install)
-  local func2=$(typeset -f symlink)
-  ssh -A -t "$@" \
-    "${func2} ;
-  ${func} ;
-  [ -r /etc/motd ] && cat /etc/motd ;
-  [ -r \"\$HOME/.profile\" ] && . \"\$HOME/.profile\" ;
-  type wellssh >/dev/null 2>&1 ||  wells_update;
-  exec env -i SSH_AUTH_SOCK=\"\$SSH_AUTH_SOCK\" \
-    SSH_CONNECTION=\"\$SSH_CONNECTION\" \
-    SSH_CLIENT=\"\$SSH_CLIENT\" SSH_TTY=\"\$SSH_TTY\" \
-    HOME=\"\$HOME\" TERM=\"\$TERM\" \
-    PATH=\"\$PATH\" SHELL=\"\$SHELL\" \
-    USER=\"\$USER\" \$SHELL -i"
-}
-
-countdown() {
-  secs=$1
-  while [ $secs -gt 0 ]; do
-    echo -ne "$secs\033[0K\r"
-    sleep 1
-  done
-}
